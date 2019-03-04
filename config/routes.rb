fd7fc81e33846c4f78d2fa1e42b8fc97b95b192a
dimitri reisinger <dimitri.reisinger@hotmail.com>
2019-03-04 18:03:03 +0100

    view show done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'boats/index'
-  get 'boats/show'
-  get 'boats/new'
-  get 'boats/create'
-  get 'boats/edit'
-  get 'boats/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
