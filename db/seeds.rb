24f8c47a822f66d45c83f32daec7be4971f56699
dimitri reisinger <dimitri.reisinger@hotmail.com>
2019-03-05 16:08:16 +0100

    fixed the countries in the view

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,11 @@ dimi = User.create(email: 'dimitri.reisinger@hotmail.com', first_name: "dimi", l
 yoni = User.create(email: 'zatalo@live.be', first_name: "yoni", last_name: "zata", password: "123456")
 thomas = User.create(email: 'thomas.cortebeeck93@gmail.com', first_name: "thomas", last_name: "cort", password: "123456")
 
-batman_url = "https://i0.wp.com/jobbiecrew.com/wp-content/uploads/2016/04/0batboat8.jpg?resize=640%2C322&ssl=1"
-
 batmoboat = Boat.create(name: "Batmo boat", country: "United States", address: "gotham", capacity: 2, description:"the bat boat", price_per_day: 50, user: User.all.sample )
 cousteauboat = Boat.create(name: "Cousteau boat", country: "France", address: "brittany", capacity: 5, description:"the red bonnet boat", price_per_day: 25, user: User.last)
 hoboeboat = Boat.create(name: "Hoboe boat", country: "Belgium", address: "molenbeek", capacity: 15, description:"you don't want to rent this", price_per_day: 2)
 titanic = Boat.create(name: "Titanic", country: "England", address: "Iceberg street", capacity: 12, description:"we might sink-hold on jack", price_per_day: 80)
 
-batmoboat.remote_photo_url = batman_url
 
 puts "Database seeded!"
 
