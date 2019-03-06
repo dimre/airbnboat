ab1147445d59f4d6093941677087291dead8ba98
dimitri reisinger <dimitri.reisinger@hotmail.com>
2019-03-06 14:11:31 +0100

    bookings done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :boat
-  validates :start_date, :end_date, :price, presence: true
+  validates :start_date, :end_date, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :boats do
   	resources :photos, only: ['new', 'create']
+    resources :bookings, only: [ 'new', 'create']
   end
+  resources :bookings, only: ['index']
 end
 
  # my_collection_boats GET    /boats/my_collection(.:format)                                                           boats#my_collection
