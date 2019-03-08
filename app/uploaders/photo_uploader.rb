92613be0e6ed4d51478e237557b98e1e87b321fd
dimitri reisinger <dimitri.reisinger@hotmail.com>
2019-03-08 14:38:07 +0100

    show done

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,12 +3,12 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   version :show do
     resize_to_fit 300, 300
-    cloudinary_transformation radius: 10
+    cloudinary_transformation radius: 10, format: :png
   end
 
   version :index do
     resize_to_fit 300, 300
-    cloudinary_transformation width: 300, height: 300, crop: :fit
+    cloudinary_transformation width: 300, height: 300, crop: :fit, format: :png
   end
 
   version :avatar do
